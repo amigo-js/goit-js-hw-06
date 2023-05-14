d845bbf8a87a8c1ae5c99ccf68adfaba0b7603af
amigo-js <120602850+amigo-js@users.noreply.github.com>
2023-05-14 21:42:02 +0200

    first task changed

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,6 @@
-const categoriesList = document.querySelector("#categories"); // getting the list of categories
-const categoriesItems = categoriesList.querySelectorAll(".item"); //getting items category
-
-console.log(`Number of ${categoriesItems.length} categories.`);
-
-categoriesItems.forEach((item) => {
-  const categoryTitle = item.querySelector("h2").textContent; // getting text of the header
-  const categoryItemsCount = item.querySelectorAll("li").length; //getting quantity of elements in category
-  console.log(`Category: ${categoryTitle} \nElements: ${categoryItemsCount}`);
-});
+const categoriesList = document.querySelectorAll('.item')
+console.log(`Number of categories:${categoriesList.length}`)
+categoriesList.forEach(e => {
+    console.log(`Category:${e.firstElementChild.textContent}`)
+    console.log(`Elements:${e.lastElementChild.querySelectorAll('li').length}`)
+})
